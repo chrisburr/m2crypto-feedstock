f49042ee59382ad70b4e73291eeb74bb8533909d
Chris Burr <christopher.burr@cern.ch>
2020-07-17 11:32:16 +0200

    Use pip for installing

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,4 @@
 
 # Use PREFIX to use conda openssl
 $PYTHON setup.py build build_ext --openssl $PREFIX
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
+$PYTHON -m pip install . -vv
